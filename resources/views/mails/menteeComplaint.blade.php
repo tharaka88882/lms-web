5ae144578d87850bc611c25084af3305a8da892e
Gaveen Wickramasinghe <gaveenwick@gmail.com>
2022-07-11 23:28:02 +0530

    Complaint email View

--- resources/views/mails/menteeComplaint.blade.php
+++ resources/views/mails/menteeComplaint.blade.php
@@ -1,8 +1,14 @@
 @component('mail::message')
-# Hello!
+# Hi (username)
 
-Mentee has filed a complaint about you.
+A Mentee has flagged a complaint as below:
+<br>
+Reson (what mentee selected in the combo box)
+<br>
+We will review and get back to you if there are any further actions required from you.
+<br>
+Please note, if there are 3 complaints flagged against your profile, your account may be blocked.
 
-Thanks,<br>
+From,<br>
 {{ config('app.name') }}
 @endcomponent
